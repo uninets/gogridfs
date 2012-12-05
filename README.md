352a78163c893fd9c5daace6a02ad46e02685d0e
Matthias Krull <m.krull@rapidrabbit.de>
2012-12-05 19:51:20 +0100

    initial

---
+++ README.md
@@ -0,0 +1,66 @@
+gogridfs
+===========
+
+A simple HTTP file server frontend to mongoDB GridFS.
+
+It can do nothing but serve files.
+
+Install
+-----------
+
+```bash
+go get github.com/mugenken/gogridfs
+```
+
+Usage
+-----------
+
+```
+gogridfs -config /path/to/config.json
+```
+
+The module is configured with a JSON file. An example may look like this:
+
+```javascript
+{
+    "servers": [                 // should contain at least one server of your mongoDB cluster
+        "localhost:27012",
+        "localhost:37012",
+        "localhost:47012"
+    ],
+    "listen": "localhost:4242",  // the host and port to listen on
+    "logfile": "gogridfs.log",   // the logfile
+    "database": "gofiles",       // the database that contains the GridFS
+    "gridfscollection": "fs",    // the GridFS root
+    "handlepath": "/gofiles/",   // the path the handler will connect to
+                                 // it will be cut from the file name so requests to
+                                 // /gofiles/some/path/file.png will serve
+                                 // some/path/file.png from GridFS
+    "debug": true                // log requested file paths
+}
+```
+
+To proxy with nginx add something like this to your server directive:
+
+```nginx
+upstream gogridfs {
+        server 127.0.0.1:4242;
+}
+
+server {
+    # more config
+
+    location /gofiles/ {
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+        if (!-f $request_filename) {
+            proxy_pass http://gogridfs;
+            break;
+        }
+    }
+
+    # more config
+}
+```
+
---
+++ gogridfs.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+    "labix.org/v2/mgo"
+    "fmt"
+    "bytes"
+    "net/http"
+    "io"
+    "io/ioutil"
+    "encoding/json"
+    "flag"
+    "log"
+    "os"
+)
+
+// file path and content
+type gridfile struct {
+    Path string
+    Data bytes.Buffer
+}
+
+// make gridfs, logger and config globally accessible
+type gogridfs struct {
+    GFS *mgo.GridFS
+    Logger *log.Logger
+    Conf config
+}
+var ggfs gogridfs
+
+// config options to unmarshaled from json
+type config struct {
+    Servers []string
+    Logfile string
+    Database string
+    GridFSCollection string
+    Listen string
+    HandlePath string
+    Debug bool
+}
+
+// load config from json file
+func loadConfig(file string) (err error) {
+
+    b_file, err := ioutil.ReadFile(file)
+    if err != nil { return }
+
+    err = json.Unmarshal(b_file, &ggfs.Conf)
+
+    return
+}
+
+// fetch file from gridfs
+func getFile (path string) (file bytes.Buffer, err error) {
+    // open gridfile where path is the filename in GridFS
+    gfsFile, err := ggfs.GFS.Open(path)
+    if err != nil {
+        return
+    }
+
+    // read file into buffer
+    for {
+        buffer := make([]byte, 4096)
+        bytes_r, err := gfsFile.Read(buffer)
+
+        if bytes_r > 0 {
+            file.Write(buffer[:bytes_r])
+        }
+
+        if err != nil { break }
+    }
+
+    // close gridfile
+    err = gfsFile.Close()
+    if err != nil {
+        return
+    }
+
+    return
+}
+
+// handle HTTP requests
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+
+    // cut handlepath from URL path
+    // remainder will be the filename to fetch from GridFS
+    path := r.URL.Path[len(ggfs.Conf.HandlePath):]
+
+    // print requested path when debugging
+    if ggfs.Conf.Debug == true {
+        ggfs.Logger.Println(path)
+    }
+
+    data, err := getFile(path)
+
+    // build the file struct
+    file := gridfile{Path: path, Data: data}
+    if err != nil {
+        ggfs.Logger.Println(err)
+    }
+
+    // print buffer to response writer
+    fmt.Fprintf(w, "%s", file.Data.String())
+}
+
+func main() {
+
+    // get config file from command line args
+    var config_file = flag.String("config", "config.json", "Config file in JSON format")
+    flag.Parse()
+
+    // load config from JSON file
+    err := loadConfig(*config_file)
+
+    // panic on errors before the log file is in place
+    if err != nil {
+        panic(err)
+    }
+
+    // initialize log writer
+    var writer io.Writer
+    if ggfs.Conf.Logfile == "" {
+        writer = os.Stdout
+    } else {
+        writer, err = os.OpenFile(ggfs.Conf.Logfile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0750)
+        // panic on errors before the log file is in place
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    ggfs.Logger = log.New(writer, "", 5)
+
+    // concatenate mongodb servers to single string of comma seperated servers
+    var servers string
+    for _, server := range ggfs.Conf.Servers {
+        servers += (server + ",")
+    }
+
+    // die if no servers are configured
+    if servers == "" {
+        ggfs.Logger.Fatalln("No mongodb servers. Please adjust your config file.")
+    }
+
+    // connect to mongodb
+    mgo_session, err := mgo.Dial(servers)
+    if err != nil {
+        ggfs.Logger.Fatalln(err)
+    }
+    defer mgo_session.Close()
+
+    // get gridfs
+    ggfs.GFS = mgo_session.DB(ggfs.Conf.Database).GridFS(ggfs.Conf.GridFSCollection)
+
+    // run webserver
+    http.HandleFunc(ggfs.Conf.HandlePath, fileHandler)
+    http.ListenAndServe(ggfs.Conf.Listen, nil)
+}
+
